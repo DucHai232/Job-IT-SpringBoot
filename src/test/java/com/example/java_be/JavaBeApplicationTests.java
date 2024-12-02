7a304fe47ebbc66a7778aa36cfe61ae984df5ee8
Duchai232 <Duchai757@gmail.com>
2024-12-02 16:54:17 +0700

    build source BE

---
+++ src/test/java/com/example/java_be/JavaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
